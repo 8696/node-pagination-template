86b92429896ebcab04289283b8c291a7c5b233b9
longjinwen <396983001@qq.com>
2019-01-10 23:51:59 +0800

    1

--- src/template.js
+++ src/template.js
@@ -1,5 +1,5 @@
 module.exports = function (options) {
-    let json = {},
+    let data = {},
         template = '',
         check = function () {
             if (Object.prototype.toString.call(options) !== '[object Object]') {
@@ -18,32 +18,104 @@ module.exports = function (options) {
                 throw new Error('options.link_url 不存在，并且类型应为String');
             }
         },
-        completeJSON = function () {
+        completeData = function () {
 
-            json.last_page = Math.ceil(options.total / options.per_page);
-            json.total = options.total;
-            json.current_page = options.current_page;
+            data.last_page = Math.ceil(options.total / options.per_page);
+            data.total = options.total;
+            data.current_page = options.current_page;
+            console.error(`总页数${data.last_page}  当前页${data.current_page}`);
+        },
+        compute = function () {
 
+            let arr = [],
+                length = data.last_page + 1;
+            for (let i = 1; i < length; i++) {
+                let page = i,
+                    type = 'link';
+                if ([1, 2].includes(page)) {
+                    arr.push({
+                        type: type,
+                        page: page
+                    });
+                } else if ([length - 1, length - 2].includes(page)) {
+                    arr.push({
+                        type: type,
+                        page: page
+                    });
+                } else if (data.last_page < 12) {
+                    // 总页数在11页以下
+                    console.log('总页数在11页以下');
+                    arr.push({
+                        type: type,
+                        page: page
+                    });
+                } else if (data.last_page >= 12) {
+                    let s = 7;
+                    if (data.last_page - data.current_page <= 2) {
+                        s += 2;
+                    }
+                    // 总页数在11页以上
+                    if (data.current_page <= s && page <= s) {
+                        arr.push({
+                            type: type,
+                            page: page
+                        });
+                    } else if (data.current_page >= length - s && page >= length - s) {
+                        arr.push({
+                            type: type,
+                            page: page
+                        });
+                    } else {
+                        let currentPage = data.current_page;
+                        if (page > currentPage - 3 && page < currentPage + 3) {
+                            arr.push({
+                                type: type,
+                                page: page
+                            });
+                        } else {
+                            if (page === 3) {
+                                arr.push({
+                                    type: 'w1',
+                                    page: '...'
+                                });
+                            } else if (length - 3 === page) {
+                                arr.push({
+                                    type: 'w2',
+                                    page: '...'
+                                });
+                            }
+
+                        }
+                    }
+                }
+
+            }
+            return arr;
         },
         completeTemplate = function () {
-            let length = json.last_page > 12 ? 12 : json.last_page + 1;
+            console.time('time');
+            let a = compute();
+            console.log(a.length);
+            console.log(a);
+            console.timeEnd('time');
 
-            for (let i = 1; i < length; i++) {
-                template += (function () {
-                    let page = i;
+            // compute().forEach(function (item) {
+            //
+            // });
+            /*    for (let i = 1; i < length; i++) {
+                    template += (function () {
+                        let page = i;
 
+                        return '<li><a href="{link}">{page}</a></li>'
+                            .replace(/{link}/, options.link_url.replace(/{page}/, page))
+                            .replace(/{page}/, page);
 
-
-                    return '<li><a href="{link}">{page}</a></li>'
-                        .replace(/{link}/, options.link_url.replace(/{page}/, page))
-                        .replace(/{page}/, page);
-
-                }());
-            }
+                    }());
+                }*/
 
             // 判断第一页 禁用
             template = `<li class="${(function () {
-                if (json.current_page === 1) {
+                if (data.current_page === 1) {
                     return 'disabled';
                 }
                 return '';
@@ -51,7 +123,7 @@ module.exports = function (options) {
 
             // 判断尾页 禁用
             template += `<li class="${(function () {
-                if (json.current_page === json.last_page) {
+                if (data.current_page === data.last_page) {
                     return 'disabled';
                 }
                 return '';
@@ -60,18 +132,15 @@ module.exports = function (options) {
             template = '<ul class="pagination">' + template + '</ul>' + '<link href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">\n';
         },
         init = function () {
-            completeJSON();
+            check();
+            completeData();
             completeTemplate();
 
         };
 
     init();
 
-
-    return {
-        template,
-        json
-    };
+    return template;
 };
 
 
--- test.js
+++ test.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const paginationTemplate = require('./main');
 
 let a = paginationTemplate({
-    current_page: 1,
-    total: 100,
-    per_page: 5,
+    current_page:8,
+    total: 15,
+    per_page: 1,
     link_url: 'http://127.0.0.1?page={page}'
 });
-fs.writeFileSync('./test.html', a.template);
+fs.writeFileSync('./test.html', a);
 
