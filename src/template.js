9f5b9bff49c5b38bd9fa1fcb153c5e9db42c93db
long <204084802@qq.com>
2019-01-11 11:52:50 +0800

    update

--- src/template.js
+++ src/template.js
@@ -17,12 +17,16 @@ module.exports = function (options) {
             if (!options.hasOwnProperty('link_url')) {
                 throw new Error('options.link_url 不存在，并且类型应为String');
             }
+            if (Math.ceil(options.total / options.per_page) < options.current_page) {
+                throw new Error(`options.current_page 不能大于总页数，总页数为${Math.ceil(options.total / options.per_page)}`);
+            }
         },
         completeData = function () {
 
             data.last_page = Math.ceil(options.total / options.per_page);
             data.total = options.total;
             data.current_page = options.current_page;
+
             console.error(`总页数${data.last_page}  当前页${data.current_page}`);
         },
         compute = function () {
@@ -31,7 +35,7 @@ module.exports = function (options) {
                 length = data.last_page + 1;
             for (let i = 1; i < length; i++) {
                 let page = i,
-                    type = 'link';
+                    type = i === data.current_page ? 'active' : 'link';
                 if ([1, 2].includes(page)) {
                     arr.push({
                         type: type,
@@ -51,9 +55,7 @@ module.exports = function (options) {
                     });
                 } else if (data.last_page >= 12) {
                     let s = 7;
-                    if (data.last_page - data.current_page <= 2) {
-                        s += 2;
-                    }
+
                     // 总页数在11页以上
                     if (data.current_page <= s && page <= s) {
                         arr.push({
@@ -75,12 +77,12 @@ module.exports = function (options) {
                         } else {
                             if (page === 3) {
                                 arr.push({
-                                    type: 'w1',
+                                    type: 'ellipsis',
                                     page: '...'
                                 });
                             } else if (length - 3 === page) {
                                 arr.push({
-                                    type: 'w2',
+                                    type: 'ellipsis',
                                     page: '...'
                                 });
                             }
@@ -93,43 +95,47 @@ module.exports = function (options) {
             return arr;
         },
         completeTemplate = function () {
-            console.time('time');
-            let a = compute();
-            console.log(a.length);
-            console.log(a);
-            console.timeEnd('time');
 
-            // compute().forEach(function (item) {
-            //
-            // });
-            /*    for (let i = 1; i < length; i++) {
-                    template += (function () {
-                        let page = i;
+            compute().forEach(function (item) {
+                template += (function () {
 
-                        return '<li><a href="{link}">{page}</a></li>'
-                            .replace(/{link}/, options.link_url.replace(/{page}/, page))
-                            .replace(/{page}/, page);
+                    switch (item.type) {
+                        case 'link':
+                            return '<li><a href="{link}">{page}</a></li>'
+                                .replace(/{link}/, options.link_url.replace(/{page}/, item.page))
+                                .replace(/{page}/, item.page);
+                        case 'ellipsis':
+                            return '<li class="disabled"><span>...</span></li>';
+                        case 'active':
+                            return '<li class="active"><span>{page}</span></li>'
+                                .replace(/{page}/, item.page);
+                    }
 
-                    }());
-                }*/
+                }());
+            });
 
-            // 判断第一页 禁用
-            template = `<li class="${(function () {
+
+            // 判断第一页 禁用 or 跳转
+            template = (function () {
                 if (data.current_page === 1) {
-                    return 'disabled';
+                    return '<li class="disabled"><span>&laquo;</span></li>';
                 }
-                return '';
-            }())}"><span>&laquo;</span></li>` + template;
+                return '<li><a href="{link}">&laquo;</a></li>'
+                    .replace(/{link}/, options.link_url.replace(/{page}/, data.current_page - 1));
 
-            // 判断尾页 禁用
-            template += `<li class="${(function () {
+            }()) + template;
+
+
+            // 判断尾页 禁用 or 跳转
+            template += (function () {
                 if (data.current_page === data.last_page) {
-                    return 'disabled';
+                    return '<li class="disabled"><span>&raquo;</span></li>';
                 }
-                return '';
-            }())}"><span>&rsaquo;</span></li>`;
+                return '<li><a href="{link}">&raquo;</a></li>'
+                    .replace(/{link}/, options.link_url.replace(/{page}/, data.current_page + 1));
+            }());
 
-            template = '<ul class="pagination">' + template + '</ul>' + '<link href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">\n';
+            template = '<ul class="pagination">' + template + '</ul>' + '<link href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">';
         },
         init = function () {
             check();
--- test.js
+++ test.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const paginationTemplate = require('./main');
 
 let a = paginationTemplate({
-    current_page:8,
-    total: 15,
-    per_page: 1,
-    link_url: 'http://127.0.0.1?page={page}'
+    current_page: 101,
+    total: 200001,
+    per_page: 2000,
+    link_url: 'http://127.0.0.1/p/{page}'
 });
 fs.writeFileSync('./test.html', a);
 
